05cfe22706b9d440c0d3757fa75e4542fabef240
FaceImplosion <faceimplosion@gmail.com>
2023-09-26 11:07:28 +0800

    Refactor functions for readability

--- write_files.py
+++ write_files.py
@@ -2,37 +2,26 @@ import replace_template
 import json
 import re
 import paths
+import config_class
 
 # Create a file for a single compressed variant of input block
 def write_blockstate(user_input, index=None):
+  config_class.dir_config
+
   i = 1
 
   if index is not None:
     i = index
 
-  print("count is specified")
-  # config_dir = paths.get_config_dir()
-  file_config = paths.get_config_dir("paths")
+  # Retrieve list of directories for necessary files
+  json_paths = read_config("paths")
 
-  file_paths = open(file_config,"r")
-  json_paths = json.load(file_paths)
-
-  # 1. Read template file
-  print("Load JSON file into variable")
-  print("Set pointer to beginning of file")
-  file_blockstates = open(paths.get_template_dir("template_blockstates"))
-  file_blockstates.seek(0)
-
-  print("Store each line inside a list:")
-  txt_blockstates = file_blockstates.readlines()
+  # Read blockstates template file
+  strings_blockstates = template_file_to_strings("template_blockstates")
 
   # Replace [template] with user input, and store later
-  new_list = replace_template.replace_lines(txt_blockstates, user_input)
+  new_list = replace_template.replace_lines(strings_blockstates, user_input)
   new_list2 = replace_template.replace_no(new_list, i)
-
-  # Write to file
-  print("Creating file...")
-  print("user_input: " + user_input)
 
   # Fetch directory for blockstates file
   dir_working = paths.get_current_dir()
@@ -55,6 +44,21 @@ def write_blockstate(user_input, index=None):
     print("ERROR: Unknown error occurred.")
   
   return
+
+def template_file_to_strings(filename):
+    file_blockstates = open(paths.get_template_dir(filename))
+    file_blockstates.seek(0)
+
+    print("Store each line inside a list:")
+    txt_blockstates = file_blockstates.readlines()
+    return txt_blockstates
+
+def read_config(filename):
+    path_file_config = paths.get_config_dir(filename)
+
+    file_paths = open(path_file_config,"r")
+    json_paths = json.load(file_paths)
+    return json_paths
 
 # Write 1x-9x variations of user input block
 def write_blockstate_all(user_input):
